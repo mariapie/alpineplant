ae02302e06e5dedea2d3e30aa2324226f5211a80
mariapie <maria.elisa.pierfederici@nmbu.no>
2021-06-22 16:44:52 +0200

    update

--- R/seedling_death.R
+++ R/seedling_death.R
@@ -1,16 +1,28 @@
-# random_death
-# kills plt for the seedling info data frame
+
+#' @title random_death
+#'
+#' create a function that randomly selct the seedling who has to die
+#'
+#' @description kills plt for the seedling info data frame
+#' @param df dataframe
+#' @param num_deaths
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' info_transform_seedling()
 random_death <- function(df, num_deaths) {
 
   deaths <- df %>%
-    filter(alive == 1) %>%
-    slice(sample(nrow(.), num_deaths)) %>%
-    pluck("plt")
+    dplyr::filter(alive == 1) %>%
+    dplyr::slice(sample(nrow(.), num_deaths)) %>%
+    purrr::pluck("plt")
   df_tmp <- df[df$plt %in% deaths,] %>%
-    mutate(alive = 0)
-  df <- bind_rows(df %>% filter(!plt %in% deaths),
+    dplyr::mutate(alive = 0)
+  df <- dplyr::bind_rows(df %>% filter(!plt %in% deaths),
                   df_tmp) %>%
-    arrange(site, year, plt)
+    dplyr::arrange(site, year, plt)
 
   return(df)
 
